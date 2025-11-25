bab2a734e7d367deb435c42f96c36ee19a057754
Darshan Hirpara <drhirpara70@gmail.com>
2025-11-25 14:55:11 +0530

    feat: update paragraph editor with action options and rebuilt text handling

--- paragraph_editor/editor.py
+++ paragraph_editor/editor.py
@@ -1,44 +1,37 @@
 """ Task 2: Modify User Paragraph """
 
-user_input = input("Enter a paragraph: ")
-split_input = user_input.split()
+import re
 
+user_input = input("Enter a Paragraph: ")
 
-def confirm_edit(msg):
-    """ Function to validate yes/no input """
+while True:
+    user_choice = input("Please enter 1 to edit, 2 to delete a word, or 3 to exit: ")
 
-    while True:
-        user_response = input(msg).lower()
-        if user_response in ["yes", "no"]:
-            return user_response
-        print("Invalid input. Please enter 'yes' or 'no'.")
+    if user_choice == '1':
+        edit_word = input("Enter the word you want to edit: ")
+        if not edit_word or not edit_word.strip():
+            print("The word to edit cannot be empty.")
+        else :
+            old_word = r'\b' + re.escape(edit_word) + r'\b'
+            if re.search(old_word, user_input):
+                new_word = input("Enter the new word: ")
+                user_input = re.sub(old_word, new_word, user_input)
+                print(user_input)
+            else :
+                print("Word not found in paragraph")
 
+    elif user_choice == '2':
+        delete_choice = input("Enter the word you want to delete: ")
+        delete_word = r'\b' + re.escape(delete_choice) + r'\b'
+        if re.search(delete_word, user_input):
+            user_input = re.sub(delete_choice, "", user_input)
+            print(user_input)
+        else :
+            print("Word not found in paragraph")
 
-def validate_input(msg):
-    """ Function to get a valid word to edit or delete """
+    elif user_choice == '3':
+        print("Exiting program.")
+        break
 
-    while True:
-        word_to_edit = input(msg)
-        if word_to_edit in split_input:
-            return word_to_edit
-        print("Word not found in the paragraph. try again.")
-
-# Edit word in the paragraph
-edit_confirmation = confirm_edit("Do you want to edit any word? (yes/no): ")
-
-if edit_confirmation == 'yes':
-    edit_choice = validate_input("Enter the word you want to edit: ")
-    if edit_choice in split_input:
-        new_word = input("Enter the new word: ")
-        index = split_input.index(edit_choice)
-        split_input[index] = new_word
-        print("Updated paragraph:", ' '.join(split_input))
-
-# Delete word from the paragraph
-delete_confirmation = confirm_edit("do you want to delete any word? (yes/no): ")
-
-if delete_confirmation == 'yes':
-    delete_choice = validate_input("Enter the word you want to delete: ")
-    if delete_choice in split_input:
-        split_input.remove(delete_choice)
-        print("Updated paragraph:", ' '.join(split_input))
+    else:
+        print("Invalid input. Please try again.")
